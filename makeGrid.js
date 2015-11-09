d09a20164a240fe6c6b4737787a32657b081b4ce
Terry <twelsh@falcon.bgsu.edu>
2015-11-08 22:03:14 -0500

    added some basic layout of the website and play area with navbar

--- makeGrid.js
+++ makeGrid.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-	mytable = $('<table></table>').attr({ id: "gridbox" });
-	var rows = 10;
-	var cols = 10;
-	var tr = [];
-	for (var i = 0; i < rows; i++) {
-		var row = $('<tr></tr>').attr({ class: ["class1", "class2", "class3"].join(' ') }).appendTo(mytable);
-		for (var j = 0; j < cols; j++) {
-			$('<td></td>').addClass('playCell').appendTo(row); 
+	var createTable = function(){	
+		mytable = $('<table></table>').attr({ id: "gridbox" });
+		var rows = 20;
+		var cols = 20;
+		var tr = [];
+		for (var i = 0; i < rows; i++) {
+			var row = $('<tr></tr>').attr({ class: ["class1", "class2", "class3"].join(' ') }).appendTo(mytable);
+			for (var j = 0; j < cols; j++) {
+				$('<td></td>').addClass('playCell').appendTo(row); 
+			}
 		}
-	}
-	mytable.appendTo("#playArea");
+		mytable.appendTo("#playGrid");
+	};
+	createTable();
 });
