5a64d671f210d3c8857af69c354c9ac684247a42
Connor Hall <Cosmokettle@Connors-MBP.attlocal.net>
2015-11-09 18:03:59 -0500

    added player move and keyHandler in js file

--- makeGrid.js
+++ makeGrid.js
@@ -23,6 +23,10 @@
 	var tileSize=15;							// tile size, in pixels
 	var playerCol=5;                                  // player starting column
 	var playerRow=4;                                  // player starting row
+	var up = false;
+	var down = false;
+	var left = false;
+	var right = false;
 	
 	//var x = Math.floor((Math.random() * 100) % 2);
 
@@ -45,13 +49,60 @@
 	// 	[1,1,0,0,0,0,0,0,0,1,1],
 	// 	[1,1,1,1,1,1,1,1,1,1,1]
 	// ];
+	
+	//handles if a key is pressed for movement
+	function keyDownHandler(e) {
+    	if(e.keyCode == 39 || e.keycode == 68) {
+        	right = true;
+    	}
+		else if(e.keyCode == 37 || e.keycode == 65) {
+        	left = true;
+    	}
+    	else if(e.keyCode == 38 || e.keycode == 87) {
+	    	up = true;
+    	}
+    	else if(e.keyCode == 40 || e.keycode == 83) {
+	    	down = true;
+    	}
+	}
+	
+	//handles if a key is not pressed
+	function keyUpHandler(e) {
+    	if(e.keyCode == 39 || e.keycode == 68) {
+        	right = false;
+    	}
+		else if(e.keyCode == 37 || e.keycode == 65) {
+        	left = false;
+    	}
+    	else if(e.keyCode == 38 || e.keycode == 87) {
+	    	up = false;
+    	}
+    	else if(e.keyCode == 40 || e.keycode == 83) {
+	    	down = false;
+    	}
+	}
+	
+	function movePlayer(){
+		if(up == true){
+			playerRow--;
+		}
+		else if(down == true){
+			playerRow++;
+		}
+		else if(left == true){
+			playerCol--;
+		}
+		else if(left == true){
+			playerCol++;
+		}
+	}
  
 	var playerYPos=playerRow*tileSize;				// converting Y player position from tiles to pixels
 	var playerXPos=playerCol*tileSize;               // converting X player position from tiles to pixels
 	
 	canvas.width=tileSize*levelCols;                   // canvas width. Won't work without it even if you style it from CSS
 	canvas.height=tileSize*levelRows;                   // canvas height. Same as before
- 
+	
 	renderLevel();
 	
 	// function to display the level
@@ -73,5 +124,8 @@
 		context.fillStyle = "#00ff00";
 		context.fillRect(playerXPos,playerYPos,tileSize,tileSize);
 	}
+	
+	
  
 })();
+
